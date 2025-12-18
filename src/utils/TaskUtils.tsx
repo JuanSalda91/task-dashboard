b77669e24fc03863945c06cc131583f1d5c7595e
JuanSalda91 <juansalda9131@gmail.com>
2025-12-18 15:40:14 -0500

    CreateTaskFormData, updateTaskForm, and filterTasks functions created

--- src/utils/TaskUtils.tsx
+++ src/utils/TaskUtils.tsx
@@ -1,1 +1,53 @@
 // ----- The functions here encapsulate transformations (filtering, sorting, localStorage), so the components can focus on UI and interactions ----- //
+import { Task, TaskFilterOptions, TaskFormData, TaskStatus, TaskPriority } from "../types"
+
+const STORAGE_KEY = "task-dashboard-tasks";
+const THEME_KEY = "task-dashboard-theme";
+
+export function createTaskFormData(data: TaskFormData): Task {
+    const now = new Date().toISOString(); // Method that converts a date's value to a string format following the ISO 8601 standard.
+    return {
+        id: crypto.randomUUID(),// UUID Helps to generate ID
+        title: data.title.trim(),
+        description: data.description.trim(),
+        status: data.status,
+        priority: data.priority,
+        dueDate: data.dueDate || undefined,
+        createdAt: now,
+        updatedAt: now,
+    };
+}
+
+export function updateTaskForm(task: Task, data: TaskFormData): Task {
+    return {
+        ...task,
+        title: data.title.trim(),
+        description: data.description.trim(),
+        status: data.status,
+        priority: data.priority,
+        dueDate: data.dueDate || undefined,
+        updatedAt: new Date(). toISOString(),
+    };
+};
+
+export function filterTasks(tasks: Task[], filters: TaskFilterOptions): Task[] {
+    return tasks.filter((task) => {
+        const matchesStatus = filters.status === "all" || task.status === filters.status;
+        const matchesPriority = filters.priority === "all" || task.priority === filters.priority;
+        const search = filters.search.trim().toLowerCase();
+        const matchesSearch =
+        !search ||
+        task.title.toLowerCase().includes(search) ||
+        task.description.toLowerCase().includes(search);
+        return matchesStatus && matchesPriority && matchesSearch;
+    });
+}
+
+
+/**
+ * - Crypto: The Crypto interface represents basic cryptography features available in the current context. It allows access to a cryptographically strong random number generator and to cryptographic primitives.
+ * 
+ * - UUID(Universally Unique Identifier): Is a label used to uniquely identify a resource among all other resources of that type. UUIDs are 128-bit values that are canonically represented as a 36-character string in the format 123e4567-e89b-12d3-a456-426614174000 (5 hex strings separated by hyphens).
+ * 
+ * - randomUUID(): The randomUUID() method of the Crypto interface is used to generate a v4 UUID using a cryptographically secure random number generator.
+ */
